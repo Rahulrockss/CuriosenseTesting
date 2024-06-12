a5d9309ddb51f3fe5d43a881ec73814d9d24361f
Rahulrockss <rahulskumar343@gmail.com>
2024-06-12 12:57:15 +0530

    Fix The UI of SideBar

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -1,40 +1,57 @@
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import {
- FaCubesStacked,
- FaUser,
- FaAddressBook,
- FaRegAddressBook,
- FaPowerOff,
- FaAddressCard,
-} from 'react-icons/fa6'
-import './Sidebar.css'
-import { UserContext } from "../App"
-import './Spinner.css'
+  FaCubesStacked,
+  FaUser,
+  FaAddressBook,
+  FaRegAddressBook,
+  FaPowerOff,
+  FaAddressCard,
+} from 'react-icons/fa6';
+import './Sidebar.css';
+import { UserContext } from "../App";
+import './Spinner.css';
 
 const Sidebar = () => {
-const {user} = useContext(UserContext) 
- const [activeLink,setActiveLink] = useState(1)
+  const { user } = useContext(UserContext);
+  const [activeLink, setActiveLink] = useState(1);
+
   return (
     <div className="sidebar">
-      <div className="sidebar-item">
-        <FaCubesStacked className="top-icon"/>
+      <div className="sidebar-item top-item">
+        <FaCubesStacked className="top-icon" />
       </div>
-      <div className={`sidebar-item ${activeLink===0 ?"active":""}`} onClick={()=>{setActiveLink(0)}}>
+      <div
+        className={`sidebar-item ${activeLink === 0 ? "active" : ""}`}
+        onClick={() => setActiveLink(0)}
+      >
         <Link className="sidebar-link">
-            <FaUser className="icon"/>{user?.name}</Link>
+          <FaUser className="icon" />{user?.name}
+        </Link>
       </div>
-      <div className={`sidebar-item ${activeLink===1 ?"active":""}`} onClick={()=>{setActiveLink(1)}}>
-        <Link to="/dashboard"className="sidebar-link">
-        <FaAddressCard className="icon"/>Game Details</Link>
+      <div
+        className={`sidebar-item ${activeLink === 1 ? "active" : ""}`}
+        onClick={() => setActiveLink(1)}
+      >
+        <Link to="/dashboard" className="sidebar-link">
+          <FaAddressCard className="icon" />Game Details
+        </Link>
       </div>
-      <div className={`sidebar-item ${activeLink===2 ?"active":""}`} onClick={()=>{setActiveLink(2)}}>
-        <Link to="/add-contact"className="sidebar-link">
-        <FaAddressCard className="icon"/>  Add Game</Link>
+      <div
+        className={`sidebar-item ${activeLink === 2 ? "active" : ""}`}
+        onClick={() => setActiveLink(2)}
+      >
+        <Link to="/add-contact" className="sidebar-link">
+          <FaAddressCard className="icon" />Add Game
+        </Link>
       </div>
-      <div className={`sidebar-item ${activeLink===3 ?"active":""}`} onClick={()=>{setActiveLink(3)}}>
-        <Link to ='/logout'className="sidebar-link">
-        <FaPowerOff className="icon"/>   Exit</Link>
+      <div
+        className={`sidebar-item ${activeLink === 3 ? "active" : ""}`}
+        onClick={() => setActiveLink(3)}
+      >
+        <Link to='/logout' className="sidebar-link">
+          <FaPowerOff className="icon" />Exit
+        </Link>
       </div>
     </div>
   );
